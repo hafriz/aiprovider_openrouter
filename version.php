fefee663a3b3781a4e0c7b2a35189c4b43c23e57
hafriz <hafriz@gmail.com>
2025-10-09 14:11:58 +0800

    Bump plugin version to 2025100801

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_openrouter';
-$plugin->version = 2024100701;
+$plugin->version = 2025100801;
 $plugin->requires = 2024100100;
 $plugin->release = '1.0.1';
 $plugin->maturity = MATURITY_STABLE;
